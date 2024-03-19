892e3aae46aca2973b2c3b145e21acd0c6f20c18
Sipsa <sipsabineesh@gmail.com>
2024-03-20 00:36:58 +0530

    installed mongoose and connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI).then(() =>{
+    console.log('Connected to MongoDB')
+})
 
 app.listen(3000,() => {
     console.log('Server listening on port 3000')
