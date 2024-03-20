6e2e8763f8b9300fec1087f8a2e0a7987163a3d7
Sipsa <sipsabineesh@gmail.com>
2024-03-20 14:58:46 +0530

    added errorhandler

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,15 @@ app.use(bodyParser.urlencoded({
  app.use('/user',userRoute)
  app.use('/auth',authRoute)
 
+  app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    })
+  })
 
 app.listen(3000,() => {
     console.log('Server listening on port 3000')
