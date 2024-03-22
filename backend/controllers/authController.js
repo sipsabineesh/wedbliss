b655d29b4c2a55fb6ff716bfa3421d00d810792b
Sipsa <sipsabineesh@gmail.com>
2024-03-22 15:28:40 +0530

    completed login functionality with error msg

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,13 +24,15 @@ export const login = async (req,res,next) =>{
          if(!validUser)  next(errorHandler(404,'User not Found'))
          const validPassword = bcryptjs.compareSync(password,validUser.password)
          if(!validPassword)  next(errorHandler(401,'Wrong Credentials'))
-         const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
-         const {password : hashedPassword,...rest} = validUser._doc
-        const expiryDate = new Date(Date.now() + 3600000)
-         res
-         .cookie('access_token',token,{httpOnly:true,expires:expiryDate})
-         .status(200)
-         .json(rest)
+         else{
+               const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+               const {password : hashedPassword,...rest} = validUser._doc
+               const expiryDate = new Date(Date.now() + 3600000)
+               res
+               .cookie('access_token',token,{httpOnly:true,expires:expiryDate})
+               .status(200)
+               .json(rest)
+          }
     } catch (error) {
         next(error)
     }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,32 +1,127 @@
-import React from 'react'
-
+import { useState } from 'react'
+import { Link ,useNavigate} from 'react-router-dom'
 export default function Login() {
-  return (
+    const [formData,setFormData] = useState({})
+    const [loading,setLoading] = useState(false)
+    const [error,setError] = useState('')
+    const navigate = useNavigate()
+    const handleChange = (e) => {
+        setFormData({...formData,[e.target.id]:e.target.value})
+    }
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        try {
+            setLoading(true)
+            setError('')
+            const res = await fetch ('/api/auth/login',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify(formData),
+        })
+          const data = await res.json()
+          console.log(data)
+          setLoading(false)
+          if(data.success === false) {
+            setError(data.message)
+          }
+          else{
+            setError('')
+            navigate('/')
+          }
+          setLoading(false)
+         
+        } catch (error) {
+            setLoading(false)
+            setError(data.message)
+        }
+       
+      
+    }
+   return (
     <>
-    <section className="login">
-    <div className="form-container">
-      <h1>Login</h1>
-      <form action="login.html">
-        <div className="control">
-          <label htmlFor="name">Name</label>
-          <input type="text" name="name" />
+    <section id="contact" className="signup contact section_padding cover-bg">
+        <div className="container">
+            <div className="row">
+                <div className="section_title text-center">
+                    {/* <p>Register For Free</p> */}
+                    <h3>Login</h3>
+                </div>
+
+                <div className="col-md-12">
+                    <div className="signup-form">
+                        <form onSubmit={handleSubmit}>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="email"
+                                               id="email"
+                                               placeholder="Email" onChange={handleChange}/>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <input type="password" 
+                                               id="password" 
+                                               placeholder="Password" onChange={handleChange}/>
+                                    </div>
+                                </div>
+                              
+                                <div className="col-md-12">
+                                    <button type="submit" disabled={loading} className="btns">{loading ? "Loading... " : "Login"}</button>
+                                </div>
+                                <div className="col-md-12 mt-4">
+                                    <div className="text-medium"><p>Don't have an account? <Link to='/signup' className='text-link'>SignUp</Link></p></div>
+                                </div>
+                                <p className='errorMsg'>{error ? error : ""}</p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="control">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" />
-        </div>
-        <p className="link">
-          <input type="checkbox" /> Remember Me
-        </p>
-        <div className="control">
-          <input type="submit" className="buttons" value="Login" />
-        </div>
-      </form>
-      <p className="link">
-        <a href="#">Forgot Password</a>
-      </p>
-    </div>
-  </section>
-  </>
+    </section>
+</>
+
   )
 }
+
+
+
+
+// import { useState } from 'react'
+// import { Link } from 'react-router-dom'
+
+// export default function Login() {
+//   return (
+//     <>
+//     <section className="login">
+//     <div className="form-container">
+//       <h1>Login</h1>
+//       <form action="login.html">
+//         <div className="control">
+//           <label htmlFor="name">Name</label>
+//           <input type="text" name="name" />
+//         </div>
+//         <div className="control">
+//           <label htmlFor="password">Password</label>
+//           <input type="password" name="password" />
+//         </div>
+//         <p className="link">
+//           <input type="checkbox" /> Remember Me
+//         </p>
+//         <div className="control">
+//           <input type="submit" className="buttons" value="Login" />
+//         </div>
+//         <div className="text-medium"><p>Don't have an account? <Link to='/signup' className='text-link'>Signup</Link></p></div>
+
+//       </form>
+//       <p className="link">
+//         <a href="#">Forgot Password</a>
+//       </p>
+//     </div>
+//   </section>
+//   </>
+//   )
+// }
