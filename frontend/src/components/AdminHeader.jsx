35e1335980200c3c80a97e2c2db0ad012de6ae6a
Sipsa <sipsabineesh@gmail.com>
2024-08-23 08:54:21 +0530

    link changed

--- frontend/src/components/AdminHeader.jsx
+++ frontend/src/components/AdminHeader.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { toast } from "react-toastify";
 import io from 'socket.io-client'
 
-const socket = io.connect('http://localhost:3000'); 
+const socket = io.connect('https://wedbliss.live'); 
 
 export default function AdminHeader() {
     const [isOpen, setIsOpen] = useState(false);
