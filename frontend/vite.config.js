cb16723c8e1dffc045f2f048d206360d9e3f2c35
Sipsa <sipsabineesh@gmail.com>
2024-08-24 20:13:00 +0530

    New Branch For Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
+ 
   server:{
     proxy:{
       '/api':{
-        target: 'https://wedbliss.live',
+        target: 'http://localhost:3000',
         secure : false,
       }
     }
