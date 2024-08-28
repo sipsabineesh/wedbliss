87b0fcbb243ee72648307b5e8195f4a972ae3a54
Sipsa <sipsabineesh@gmail.com>
2024-08-28 11:46:19 +0530

    CORRECTION IN VITE CONFIG FOR DEPLOYMT

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'http://localhost:3000',
+//         secure : false,
+//       }
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://wedbliss.live',
-        secure : false,
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://wedbliss.live',
+        changeOrigin: true,  // Add this line
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')  // Optional, depending on your API endpoint
       }
     }
   }
