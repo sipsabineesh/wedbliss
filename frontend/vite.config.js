250e2bb39ff381a47c5697311651b181fada0c75
Sipsa Bineesh <132487161+sipsabineesh@users.noreply.github.com>
2024-08-28 11:44:21 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'https://wedbliss.live',
+//         secure : false,
+//       }
+//     }
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'https://wedbliss.live',
-        secure : false,
+        changeOrigin: true,  // Add this line
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')  // Optional, depending on your API endpoint
       }
     }
   }
