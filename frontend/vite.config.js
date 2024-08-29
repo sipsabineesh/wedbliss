8b86d88882c87b0cdde8d5c1d4b1d38df4b5e7f2
Sipsa <sipsabineesh@gmail.com>
2024-08-29 11:00:11 +0530

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
+  server:{
+    proxy:{
+      '/api':{
         target: 'https://wedbliss.live',
-        changeOrigin: true,  // Add this line
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')  // Optional, depending on your API endpoint
+        secure : false,
       }
     }
   }
